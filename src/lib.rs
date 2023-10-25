cec208200dec8093d3afc48c223dd15fe98116c9
Chip Reed <chip@crabnebula.dev>
2023-10-25 09:47:42 +0200

    step 1

---
+++ src/lib.rs
@@ -0,0 +1,19 @@
+pub use phf; // re-export phf so we can use it later
+
+type Map = phf::Map<&'static str, &'static str>;
+
+/// Container for compile-time embedded assets
+pub struct Assets(Map);
+
+impl From<Map> for Assets {
+    fn from(value: Map) -> Self {
+        Self(value)
+    }
+}
+
+impl Assets {
+    /// Fetch the contents of the specified asset path
+    pub fn fetch(&self, path: &str) -> Option<&str> {
+        self.0.get(path).copied()
+    }
+}
