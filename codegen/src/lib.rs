5d7cd6ab16bf39cfa365cef7896f49f74791971d
Chip Reed <chip@crabnebula.dev>
2023-11-02 12:40:05 +0100

    use less iterators on gather_asset_paths

--- codegen/src/lib.rs
+++ codegen/src/lib.rs
@@ -14,14 +14,15 @@ pub fn codegen(path: &Path) -> std::io::Result<String> {
 
 /// Recursively find all files in the passed directory.
 fn gather_asset_paths(base: &Path) -> Vec<PathBuf> {
-    WalkDir::new(base)
-        .contents_first(true)
-        .into_iter()
-        // ignore failed entries for this example
-        .flatten()
+    let mut paths = Vec::new();
+    for entry in WalkDir::new(base).into_iter().flatten() {
         // we only care about files, ignore directories
-        .flat_map(|entry| entry.file_type().is_file().then(|| entry.into_path()))
-        .collect()
+        if entry.file_type().is_file() {
+            paths.push(entry.into_path())
+        }
+    }
+
+    paths
 }
 
 /// Generate Rust code to create an [`asset-bundler::Asset`].
